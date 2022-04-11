a09be69099bcdb0d0c2672f82d931a464f1da841
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2022-04-11 12:02:00 +0200

    dixed all order user and my order

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -25,7 +25,7 @@ export class OrderService {
 
   getOrdersByUser(userId: string) {
     return this.db
-      .list('/orders', (ref) => ref.orderByChild('userId').equalTo(userId))
+      .list('/orders', (ref) => ref.orderByChild('user/userId').equalTo(userId))
       .valueChanges();
   }
 
